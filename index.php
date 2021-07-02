0e2db40b43fd33d30b25e18da2c7185e2bfbb174
Aditya Bodhankae <adityaabodhankar@gmail.com>
2021-07-02 16:15:21 +0530

    login and header

--- index.php
+++ index.php
@@ -1,141 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aditya Bodhankar</title>
-    <style>
-        .font_4 {
-            font: normal normal normal 48px 'playfair display', serif;
-            color: white;
-        }
-
-        html {
-            scroll-behavior: smooth;
-        }
-
-        body {
-            font-family: 'Montserrat', sans-serif;
-            background: linear-gradient(rgba(1, 1, 1, 0.8), rgba(1, 1, 1, 0.9)), url("./images/background.jpg");
-            background-color: #5e5e5e;
-            /* Used if the image is unavailable */
-            background-position: center;
-            /* Center the image */
-            background-attachment: fixed;
-            background-repeat: no-repeat;
-            /* Do not repeat the image */
-            background-size: cover;
-            /* Resize the background image to cover the entire container */
-            color: #FFFFFF;
-        }
-
-        .bg-50 {
-            background-color: #00000077;
-        }
-        
-        .vert .carousel-item-next.carousel-item-left,
-        .vert .carousel-item-prev.carousel-item-right {
-            -webkit-transform: translate3d(0, 0, 0);
-                    transform: translate3d(0, 0, 0);
-        }
-
-        .vert .carousel-item-next,
-        .vert .active.carousel-item-right {
-            -webkit-transform: translate3d(0, 100%, 0);
-                    transform: translate3d(0, 100% 0);
-        }
-
-        .vert .carousel-item-prev,
-        .vert .active.carousel-item-left {
-        -webkit-transform: translate3d(0,-100%, 0);
-                transform: translate3d(0,-100%, 0);
-        }
-
-    </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-
-<body>
-
-    <div class="px-3 py-2 text-white bg-50">
-        <div class="container">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                    <h5>Aditya Bodhankar</h5>
-                </a>
-
-                <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                <svg id="home" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"></path>
-                                </svg>
-                            </svg>
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                <svg id="speedometer2" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"></path>
-                                    <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"></path>
-                                </svg>
-                            </svg>
-                            Dashboard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                <svg id="grid" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"></path>
-                                </svg>
-                            </svg>
-                            Products
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                </svg>
-                            </svg>
-                            Login
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <div class="container">
-        <img id="tilt" class="rounded-circle d-block mx-auto m-3 aos-init aos-animate" src="Images/background.jpg" height="256" width="256" data-aos="zoom-in-up">
+<?php include "./header.php"; ?>
+<div class="container">
+    <img id="tilt" class="rounded-circle d-block mx-auto m-3 aos-init aos-animate" src="Images/background.jpg" height="256" width="256" data-aos="zoom-in-up">
+    <br>
+    <center>
+        <h3 class="font_4">
+            Aditya Ashok Bodhankar
+        </h3>
         <br>
-        <center>
-            <h3 class="font_4">
-                Aditya Ashok Bodhankar
-            </h3>
-            <br>
-            <a class="btn" target="_blank" href="https://www.instagram.com/ab_aditya_26">
-                <img src="./images/insta.png" style="max-width: 40px;">
-            </a>
-            <a class="btn" target="_blank" href="https://www.facebook.com/aditya.bodhankar.90/">
-                <img src="./images/fb.png" style="max-width: 40px;">
-            </a>
-            <a class="btn" target="_blank" href="https://wa.me/917057974721">
-                <img src="./images/whatsapp.png" style="max-width: 40px;">
-            </a>
-            <a class="btn" target="_blank" href="https://t.me/ab_aditya_26">
-                <img src="./images/telegram.png" style="max-width: 40px;">
-            </a>
-            <br>
-            <!-- <div id="carouselExampleSlidesOnly" style="max-width:300px;" class="carousel vert slide" data-ride="carousel">
+        <a class="btn" target="_blank" href="https://www.instagram.com/ab_aditya_26">
+            <img src="./images/insta.png" style="max-width: 40px;">
+        </a>
+        <a class="btn" target="_blank" href="https://www.facebook.com/aditya.bodhankar.90/">
+            <img src="./images/fb.png" style="max-width: 40px;">
+        </a>
+        <a class="btn" target="_blank" href="https://wa.me/917057974721">
+            <img src="./images/whatsapp.png" style="max-width: 40px;">
+        </a>
+        <a class="btn" target="_blank" href="https://t.me/ab_aditya_26">
+            <img src="./images/telegram.png" style="max-width: 40px;">
+        </a>
+        <br>
+        <!-- <div id="carouselExampleSlidesOnly" style="max-width:300px;" class="carousel vert slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item">
                         <img src="./images/web.jpg" width="30px" height="30px">
@@ -151,8 +36,57 @@
                     </div>
                 </div>
             </div> -->
-        </center>
-    </div>
+    </center>
+    <hr class="hr" style="border-color: white;">
+    <br><br>
+    <section id="projects" style="padding-top: 20px; padding-bottom: 20px;">
+        <div class="container">
+            <center>
+                <h2 style="font-family: Impact, Chrcoal, sans-serif;">
+                    <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-file-earmark-zip-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M2 3a2 2 0 0 1 2-2h.5v1h1v1h-1v1h1v1h-1v1h1v1H7V6H6V5h1V4H6V3h1V2H6V1h3.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zM5.5 7.5a1 1 0 0 0-1 1v.938l-.4 1.599a1 1 0 0 0 .416 1.074l.93.62a1 1 0 0 0 1.109 0l.93-.62a1 1 0 0 0 .415-1.074l-.4-1.599V8.5a1 1 0 0 0-1-1h-1zm0 1.938V8.5h1v.938a1 1 0 0 0 .03.243l.4 1.598-.93.62-.93-.62.4-1.598a1 1 0 0 0 .03-.243z"></path>
+                    </svg>
+                    Recent Project
+
+                </h2><br><br>
+
+
+                <div class="row-img-1 row" onclick="openfile('./viewimages.php?img=images/Aditya Bodhankar/projects/296825309137')" style="margin:10px; padding:20px;">
+                    <div class="col-md-5">
+                        <div class="from-left slide-in disappear">
+                            <img src="images/background.jpg" class="img-projects">
+                        </div>
+                    </div>
+                    <div class="col-md-5 from-right slide-in disappear" style="padding-top:20px">
+                        <h5 class="">
+                            Shield Website </h5>
+                        <p class="">
+                        </p>
+                        <p>Designed by:</p>
+
+                        <p>Aditya Bodhankar</p>
+
+                        <p>Kalpak Nemade</p>
+
+                        <p>Vishal Chaudhari</p>
+
+                        <p>Mahesh Pimparkar</p>
+                        <p></p>
+                    </div>
+                </div>
+                <hr style="height:2px;border-width:2;color:white;background-color:white">
+
+                <br>
+                <a href="./projects.php" class="btn-a">
+                    <h3>+ <small>View More</small></h3>
+                </a>
+
+
+
+            </center>
+        </div>
+    </section>
+</div>
 
 </body>
 
@@ -167,4 +101,4 @@
         // })
 
     </script>
-        -->
+-->
---
+++ login.php
@@ -0,0 +1,29 @@
+<?php include "./header.php"; ?>
+    <br><br>
+    <main class="form-signin">
+        <form action="login.php" method="POST">
+            <center>
+                <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                </svg>
+            </center>
+            <br>
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" autocomplete="off">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+    </main>
+
+
+</body>
+
+</html>
