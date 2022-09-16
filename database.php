5ee081b49be8cb7bcdc5fb19c56319e118adb19f
Aditya Bodhankar <62885184+abaditya26@users.noreply.github.com>
2022-09-16 18:18:04 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,2 +1,2 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'aditya_portfolio');
+$conn = mysqli_connect('ec2-52-86-115-245.compute-1.amazonaws.com', 'qlylekeehlvbeu', 'cde9522faac7b3a7c67fbf528746de07a2c2710c14ee5ac36a51110811fa19e8', 'ddsr7g9q4e9g4l');
